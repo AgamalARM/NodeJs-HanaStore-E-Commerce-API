e8c6f544effd0ea4c3e0df78bc443c249c7d0c16
Ubuntu <ubuntu@ip-172-31-18-166.eu-north-1.compute.internal>
2023-10-22 17:16:22 +0000

    morgan logger middelware

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config({path: 'config.env'});
+
 const app = express();
 
+//middelware Before the routes
+
+//if I want to use morgan only in developement mode
+if (process.env.NODE_ENV === 'developement'){
+    app.use(morgan('dev')); // morgan middelware for logger
+    console.log(`mode: ${process.env.NODE_ENV}`);
+    
+}
+
+
+//Routes
 app.get('/', (req,res) => {
-    res.send('<h1>الله أكبر</h1>');
+    res.send('<h1> API V1</h1>');
 })
 
 const PORT = process.env.PORT || 8080 ;
